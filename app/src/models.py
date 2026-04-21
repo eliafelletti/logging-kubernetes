e45953a77aff87b7c24d1d98679a46b8f029d218
Elia Felletti <elia.felletti@gmail.com>
2026-04-21 22:26:42 +0200

    add SQLAlchemy User model

---
+++ app/src/models.py
@@ -0,0 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from dotenv import load_dotenv
+
+'''
+    This module defines the database models for the application. It uses SQLAlchemy to create a User model with fields for id, username, email, and created_at. 
+    The User model also includes a method to convert the object to a dictionary for easy JSON serialization and a string representation for debugging purposes.
+'''
+
+# DB initialization
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users' # Table name in the database
+
+    load_dotenv() # Load environment variables from a .env file if present
+
+    # Columns
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.timezone.utc.now)
+
+    # Method to convert the User object to a JSON dictionary for API responses
+    def __todict__(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'created_at': self.created_at.isoformat()
+        }
+    
+    # String representation of the User object for debugging purposes
+    def __repr__(self):
+        return f'<User {self.username}>'
